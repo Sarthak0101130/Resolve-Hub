6fcd4dee00a4102b2d680d07a6b1c06cdd714651
Sarthak0101130 <4730.sarthakbansal@gmail.com>
2024-02-22 22:10:30 +0530

    otp_verification_done 22/02/2024 22:10

---
+++ complain_management/app/src/main/java/com/example/complain_management/dashboard.kt
@@ -0,0 +1,12 @@
+package com.example.otpverification
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.complain_management.R
+
+class dashboard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
